a9d2dfd3dd26208cad8386bc71b962e0cbf14ce3
kunal srivastav <shrivastavkunal56@gmail.com>
2025-10-05 07:34:54 -0700

    Add About and Contact images

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -8,7 +8,7 @@ function Admin() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="d-flex flex-column" style={{ minHeight: "auto" }}>
+    <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
       {/* Header */}
       <header className="d-flex justify-content-between align-items-center p-3 border-bottom bg-light">
         <Link to="/"
@@ -30,8 +30,8 @@ function Admin() {
       <div className="d-flex flex-grow-1 position-relative">
         {/* Sidebar */}
         <div
-          className={`bg-light w-50 border-end d-flex flex-column p-3 ${
-            sidebarOpen ? "d-flex position-fixed start-0 top-0 z-3" : "d-none d-md-flex"
+          className={`bg-light border-end d-flex flex-column p-3 ${
+            sidebarOpen ? "d-flex position-fixed start-0 top-0 z-3 w-50" : "d-none d-md-flex"
           }`}
           style={{
             width: sidebarOpen ? "100vw" : "200px",

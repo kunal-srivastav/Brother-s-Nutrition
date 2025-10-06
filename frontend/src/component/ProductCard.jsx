19f0543334b12c3e3b98ff9d1a462d08281d4e49
kunal srivastav <shrivastavkunal56@gmail.com>
2025-10-06 00:47:57 -0700

    Fix UI of product category

--- frontend/src/component/ProductCard.jsx
+++ frontend/src/component/ProductCard.jsx
@@ -6,36 +6,32 @@ function ProductCard ({product}) {
     const { loggedInUser } = useSelector(state => state.users);
 
     return (
-    <div className="col-6 col-sm-4 col-md-3 col-lg-2 d-flex" key={product?._id}>
-    <div className="product-card card flex-fill shadow-sm border-0 d-flex flex-column h-100">
-        <Link
-        to={loggedInUser ? `/product/${product?._id}` : "/login"}
-        className="text-decoration-none text-dark flex-grow-1"
-        >
-        <div className="product-image position-relative text-center">
-            <img
-            src={product?.images[0]}
-            loading="lazy"
-            alt={product?.name}
-            className="img-fluid"
-            />
-            {product.discount && (
-            <span className="ribbon">{product?.discount}% OFF</span>
-            )}
+        <div className="col d-flex" key={product?._id}>
+            <div className="product-card card flex-fill shadow-sm border-0 d-flex flex-column h-100">
+            <Link to={loggedInUser ? `/product/${product?._id}` : "/login"}
+                className="text-decoration-none text-dark flex-grow-1" >
+                <div className="product-image position-relative text-center">
+                <img src={product?.images[0]} loading="lazy"
+                    alt={product?.name} className="img-fluid" />
+                    {product.discount && (
+                    <span className="ribbon">
+                      {product?.discount}% OFF
+                    </span>
+                    )}
+                </div>
+            </Link>
+            <div className="p-3 mt-auto">
+                <p className="fw-semibold product-name text-truncate mb-1">
+                {product?.name}
+                </p>
+                <div>
+                <span className="fw-bold fs-5 text-dark">
+                    ₹{product?.finalPrice}
+                </span>
+                </div>
+            </div>
+            </div>
         </div>
-        </Link>
-
-        <div className="p-3 mt-auto">
-        <p className="fw-semibold product-name text-truncate mb-1">
-            {product?.name}
-        </p>
-        <div>
-            <span className="fw-bold fs-5 text-dark">₹{product?.finalPrice}</span>
-        </div>
-        </div>
-    </div>
-    </div>
-
     )
 };
 
